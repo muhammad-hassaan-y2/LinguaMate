bf26e8f9e827b1bd8606fed4db79d95b5fac89e6
Victor Ezealor <75592515+uzochukwuV@users.noreply.github.com>
2024-11-21 05:37:57 -0800

    debug: postgres migration issue

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -8,6 +8,7 @@ config({
 });
 
 const runMigrate = async () => {
+  
   if (!process.env.POSTGRES_URL) {
     throw new Error('POSTGRES_URL is not defined');
   }
@@ -18,7 +19,8 @@ const runMigrate = async () => {
   console.log('⏳ Running migrations...');
 
   const start = Date.now();
-  await migrate(db, { migrationsFolder: './lib/drizzle' });
+  // Noticed you used drizzle here but the migrations folder was set to migrations
+  await migrate(db, { migrationsFolder: __dirname + '/migrations' }).then(()=> connection.end());
   const end = Date.now();
 
   console.log('✅ Migrations completed in', end - start, 'ms');
